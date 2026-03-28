ee8d8cd3bc55a120fc51366d6e7a96af5bb764f5
FrontendCourseMP <rasskazovaekaterina20@gmail.com>
2026-03-28 23:12:57 +0300

    основа

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Калькулятор</title>
+</head>
+<body>
+    <h1>Калькулятор</h1>
+</body>
+</html>
