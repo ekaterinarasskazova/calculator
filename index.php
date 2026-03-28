ec51e25186eb2fa20a2bb29cb224bfdfb9342de8
FrontendCourseMP <rasskazovaekaterina20@gmail.com>
2026-03-28 23:34:36 +0300

    кнопки калькулятора

--- index.php
+++ index.php
@@ -11,7 +11,39 @@ session_start();
     <h1>Калькулятор</h1>
     <form method="POST" action="index.php">
         <input type="text" id="display" name="val" readonly>
-        <button type="submit">=</button>
+        
+        <div>
+            <button type="button" onclick="appendValue('7')">7</button>
+            <button type="button" onclick="appendValue('8')">8</button>
+            <button type="button" onclick="appendValue('9')">9</button>
+            <button type="button" onclick="appendValue('/')">/</button>
+        </div>
+
+        <div>
+            <button type="button" onclick="appendValue('4')">4</button>
+            <button type="button" onclick="appendValue('5')">5</button>
+            <button type="button" onclick="appendValue('6')">6</button>
+            <button type="button" onclick="appendValue('*')">*</button>
+        </div>
+
+        <div>
+            <button type="button" onclick="appendValue('1')">1</button>
+            <button type="button" onclick="appendValue('2')">2</button>
+            <button type="button" onclick="appendValue('3')">3</button>
+            <button type="button" onclick="appendValue('-')">-</button>
+        </div>
+
+        <div>
+            <button type="button" onclick="appendValue('0')">0</button>
+            <button type="button" onclick="appendValue('(')">(</button>
+            <button type="button" onclick="appendValue(')')">)</button>
+            <button type="button" onclick="appendValue('+')">+</button>
+        </div>
+
+        <div>
+            <button type="button" onclick="clearDisplay()">C</button>
+            <button type="submit">=</button>
+        </div>
     </form>
 </body>
 </html>
